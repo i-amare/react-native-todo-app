c3a94a0310209a1d0517e1be085b7836516c1c0b
Nsovo Ngobeni <97952201+i-amare@users.noreply.github.com>
2023-07-25 12:29:16 +0200

    added completion property and associated methods

--- types.ts
+++ types.ts
@@ -1,10 +1,12 @@
 export class Todo {
 	private title: string;
 	private id: string;
+	private completion: boolean;
 
 	constructor(title: string) {
 		this.title = title;
 		this.id = `${title}-${Math.round(Math.random() * 100000)}`;
+		this.completion = false;
 	}
 
 	getTitle() {
@@ -15,7 +17,19 @@ export class Todo {
 		return this.id;
 	}
 
+	isCompleted() {
+		return this.completion;
+	}
+
 	setTitle(title: string) {
 		this.title = title;
+	}
+
+	setCompleted(val: boolean) {
+		this.completion = val;
+	}
+
+	toggleCompletion() {
+		this.completion = !this.completion;
 	}
 }
